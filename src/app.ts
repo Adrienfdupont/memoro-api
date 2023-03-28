cf3c311a0a2e3e2f047dcf194a49c663c7af3863
Adrienfdupont <adrienf.dupont@gmail.com>
2023-03-28 23:55:05 +0200

    inscription ok et connexion pour login ok

--- src/app.ts
+++ src/app.ts
@@ -1,66 +1,67 @@
 import express from "express";
+import mariadb, { Connection, SqlError } from "mariadb";
 import bodyParser from "body-parser";
-import mysql from "mysql";
 import bcrypt, { hash } from "bcrypt";
 
 const app = express();
 
-// const connection = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "easy-learn",
-// });
-
-app.use("/test_static", express.static("public"));
-app.use(bodyParser.json());
-
-// app.post("/api/user", (req, res) => {
-//   const saltRounds = 10;
-//   bcrypt.hash(req.body.password, saltRounds, (error, hash) => {
-//     if (error) {
-//       console.error("Error hashing password: ", error);
-//       return;
-//     }
-
-//     connection.connect();
-
-//     connection.query(
-//       {
-//         sql: "INSERT INTO users (username, password) VALUES (?,?)",
-//         values: [req.body.username, hash],
-//       },
-//       (error, results) => {
-//         console.error(error);
-//         console.log(results);
-//         connection.end();
-//         res.send("utilisateur inséré");
-//       }
-//     );
-//   });
-// });
-
-const pool = mysql.createPool({
-  connectionLimit: 10,
-  host: "localhost",
+const pool = mariadb.createPool({
+  host: process.env.DB_HOST ?? "localhost",
   user: "root",
-  password: "",
+  password: "root",
   database: "easy-learn",
+  connectionLimit: 5,
 });
 
-app.post("/api/user", (req, res) => {
-  pool.getConnection((err, connection) => {
-    if (err) throw err;
+let connection: Connection | undefined = undefined;
 
-    connection.query("SELECT * FROM users", (error, results, fields) => {
-      connection.release(); // libérer la connexion
+app.use(bodyParser.json());
 
-      if (error) throw error;
 
-      // traiter les résultats de la requête
-      console.log(results);
+app.post("/api/user", async (req, res) => {
+  try {
+    connection = await pool.getConnection();
+    const saltRounds = 10;
+    const hash = await bcrypt.hash(req.body.password, saltRounds);
+    const response = await connection.query(
+      "INSERT INTO users(name, password) VALUES(?, ?)",
+      [req.body.username, hash]
+    );
+    res.send({
+      message: "Vous avez bien été inscrit."
     });
-  });
+  } catch (err) {
+    res.status(500);
+    if (err instanceof SqlError){
+      if (err.errno === 1062){
+        res.send({
+          "message": "Ce nom est déjà utilisé."
+        })
+      }
+    }
+  }
+  res.send();
+  connection?.end();
+});
+
+
+app.post("/api/login", async (req, res) => {
+  try {
+    connection = await pool.getConnection();
+    const response = await connection.query(
+      "SELECT * FROM users WHERE name = ?",
+      [req.body.username]
+    );
+    res.send(response);
+  } catch (err) {
+    res.status(500);
+    console.log(err);
+    res.send({
+      message: "erreur"
+    });
+  }
+  res.send();
+  connection?.end();
 });
 
 app.listen(3000, () => console.log("Serveur démarré"));
