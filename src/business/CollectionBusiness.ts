adcab2f4f8c4a246c352363aa3286c0056b0ac62
Adrienfdupont <adrienf.dupont@gmail.com>
2023-06-27 21:29:02 +0200

    collections implemented

--- src/business/CollectionBusiness.ts
+++ src/business/CollectionBusiness.ts
@@ -4,18 +4,30 @@ import ConnectionHelper from "../helper/ConnectionHelper";
 import Collection from "../types/Collection";
 
 export default class CollectionBusiness {
+  static async addCollection(name: string, userId: number): Promise<void> {
+    const sql = "INSERT INTO collections (name, user_id) VALUES (?, ?)";
+    const placeholders = [name, userId.toString()];
+    let sqlResults: any;
+
+    try {
+      sqlResults = await ConnectionHelper.performQuery(sql, placeholders);
+    } catch (err) {
+      if (err instanceof SqlError && err.errno === 1062) {
+        throw new StatusMsgError(409, "The collection already exists for this user.");
+      }
+    }
+
+    if (sqlResults.affectedRows === 0) {
+      throw new StatusMsgError(500, "The collection could not be added.");
+    }
+  }
+
   static async getCollections(authUserId: number): Promise<Collection[]> {
     const sql = "SELECT collections.* FROM collections INNER JOIN users ON collections.user_id = users.id";
     const placeholders = [authUserId.toString()];
-    let queryResults: any[];
+    const queryResults: any[] = await ConnectionHelper.performQuery(sql, placeholders);
 
-    try {
-      queryResults = await ConnectionHelper.performQuery(sql, placeholders);
-    } catch (err) {
-      throw new StatusMsgError(500, "Internal server error.");
-    }
-
-    const collections: Collection[] = queryResults.map((result) => {
+    const collections = queryResults.map((result) => {
       return {
         id: result.id,
         name: result.name,
@@ -29,13 +41,7 @@ export default class CollectionBusiness {
   static async getCollection(collectionId: string): Promise<Collection> {
     const sql = "SELECT * FROM collections WHERE id = ?";
     const placeholders = [collectionId];
-    let queryResults: any[];
-
-    try {
-      queryResults = await ConnectionHelper.performQuery(sql, placeholders);
-    } catch (err) {
-      throw new StatusMsgError(500, "Internal server error.");
-    }
+    const queryResults: any = await ConnectionHelper.performQuery(sql, placeholders);
 
     const collection = {
       id: queryResults[0].id,
@@ -46,28 +52,7 @@ export default class CollectionBusiness {
     return collection;
   }
 
-  static async addCollection(name: string, userId: number): Promise<void> {
-    if (name.length === 0) {
-      throw new StatusMsgError(400, "Please provide a name.");
-    }
-    const sql = "INSERT INTO collections (name, user_id) VALUES (?, ?)";
-    const placeholders = [name, userId.toString()];
-    let sqlResults: any;
-
-    try {
-      sqlResults = await ConnectionHelper.performQuery(sql, placeholders);
-    } catch (err) {
-      if (err instanceof SqlError && err.errno === 1062) {
-        throw new StatusMsgError(409, "You already own a collection with this name.");
-      }
-    }
-  }
-
   static async updateCollection(collectionId: string, name: string): Promise<void> {
-    if (name.length === 0) {
-      throw new StatusMsgError(400, "Please provide a name.");
-    }
-
     const sql = "UPDATE collections SET name = ? WHERE id = ?";
     const placeholders = [name, collectionId];
     let sqlResults: any;
@@ -76,20 +61,18 @@ export default class CollectionBusiness {
       sqlResults = await ConnectionHelper.performQuery(sql, placeholders);
     } catch (err) {
       if (err instanceof SqlError && err.errno === 1062) {
-        throw new StatusMsgError(409, "You already own a collection with this name.");
+        throw new StatusMsgError(409, "The collection already exists for this user.");
       }
+    }
+
+    if (sqlResults.affectedRows === 0) {
+      throw new StatusMsgError(500, "The collection could not be updated.");
     }
   }
 
   static async removeCollection(collectionId: string): Promise<void> {
     const sql = "DELETE FROM collections WHERE id = ?";
     const placeholders = [collectionId];
-    let sqlResults: any;
-
-    try {
-      sqlResults = await ConnectionHelper.performQuery(sql, placeholders);
-    } catch (err) {
-      throw new StatusMsgError(500, "Internal server error.");
-    }
+    await ConnectionHelper.performQuery(sql, placeholders);
   }
 }
