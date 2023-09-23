c303e5d9f82e549e91666a47e7b47650dfa97a39
Adrienfdupont <adrienf.dupont@gmail.com>
2023-09-23 14:37:36 +0200

    Revert "test serverless functions"
    This reverts commit 63d5e5e15e46ceb991c3f6a818cfdc842dd8d908.

--- api/index.ts
+++ api/index.ts
@@ -25,4 +25,39 @@ const cardController = new CardController();
 
 app.get('/api/', (req, res) => res.send('Momoro APi is workling.'));
 
+app.post('/api/user/register', (req, res) => userController.register(req, res));
+app.post('/api/user/login', (req, res) => userController.login(req, res));
+
+// routes that need authentication
+
+app.use(SecurityHelper.middleware);
+
+app.get('/api/user/:id', (req, res) => userController.getUser(req, res));
+app.put('/api/user', (req, res) => userController.updateUser(req, res));
+app.post('/api/user/delete', (req, res) => userController.removeUser(req, res));
+
+app.post('/api/card', (req, res) => cardController.addCard(req, res));
+app.get('/api/cards/collection/:id', (req, res) =>
+  cardController.getCards(req, res)
+);
+app.get('/api/card/:id', (req, res) => cardController.getCard(req, res));
+app.put('/api/card', (req, res) => cardController.updateCard(req, res));
+app.delete('/api/card/:id', (req, res) => cardController.removeCard(req, res));
+
+app.post('/api/collection', (req, res) =>
+  collectionController.addCollection(req, res)
+);
+app.get('/api/collections/user/:id', (req, res) =>
+  collectionController.getCollections(req, res)
+);
+app.get('/api/collection/:id', (req, res) =>
+  collectionController.getCollection(req, res)
+);
+app.put('/api/collection', (req, res) =>
+  collectionController.updateCollection(req, res)
+);
+app.delete('/api/collection/:id', (req, res) =>
+  collectionController.removeCollection(req, res)
+);
+
 export default app;
