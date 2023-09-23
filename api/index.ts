687bd4b4a0ff870d5b2808c52bb8a44c5b5458d5
Adrienfdupont <adrienf.dupont@gmail.com>
2023-09-23 14:35:50 +0200

    Revert "test serverless funcitons"
    This reverts commit fe23c714bc330a8086b517b02dfec27aef109658.

--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,27 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import ConnectionHelper from './helpers/ConnectionHelper';
+import SecurityHelper from './helpers/SecurityHelper';
+import UserController from './controllers/UserController';
+import CardController from './controllers/CardController';
+import CollectionController from './controllers/CollectionController';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
 const app = express();
 
 dotenv.config();
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(
   cors({
     origin: process.env.ALLOW_ORIGIN,
   })
 );
+ConnectionHelper.createPool();
+
+const userController = new UserController();
+const collectionController = new CollectionController();
+const cardController = new CardController();
 
 app.get('/api/', (req, res) => res.send('Momoro APi is workling.'));
 
