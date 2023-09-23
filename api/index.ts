f394f36e9106a5433c71c2d28e78c10787161499
Adrienfdupont <adrienf.dupont@gmail.com>
2023-09-23 14:21:30 +0200

    remove async on routes

--- api/index.ts
+++ api/index.ts
@@ -23,46 +23,40 @@ const userController = new UserController();
 const collectionController = new CollectionController();
 const cardController = new CardController();
 
-app.get('/api/', async (req, res) => res.send('Momoro APi is workling.'));
+app.get('/api/', (req, res) => res.send('Momoro APi is workling.'));
 
-app.post('/api/user/register', async (req, res) =>
-  userController.register(req, res)
-);
-app.post('/api/user/login', async (req, res) => userController.login(req, res));
+app.post('/api/user/register', (req, res) => userController.register(req, res));
+app.post('/api/user/login', (req, res) => userController.login(req, res));
 
 // routes that need authentication
 
 app.use(SecurityHelper.middleware);
 
-app.get('/api/user/:id', async (req, res) => userController.getUser(req, res));
-app.put('/api/user', async (req, res) => userController.updateUser(req, res));
-app.post('/api/user/delete', async (req, res) =>
-  userController.removeUser(req, res)
-);
+app.get('/api/user/:id', (req, res) => userController.getUser(req, res));
+app.put('/api/user', (req, res) => userController.updateUser(req, res));
+app.post('/api/user/delete', (req, res) => userController.removeUser(req, res));
 
-app.post('/api/card', async (req, res) => cardController.addCard(req, res));
-app.get('/api/cards/collection/:id', async (req, res) =>
+app.post('/api/card', (req, res) => cardController.addCard(req, res));
+app.get('/api/cards/collection/:id', (req, res) =>
   cardController.getCards(req, res)
 );
-app.get('/api/card/:id', async (req, res) => cardController.getCard(req, res));
-app.put('/api/card', async (req, res) => cardController.updateCard(req, res));
-app.delete('/api/card/:id', async (req, res) =>
-  cardController.removeCard(req, res)
-);
+app.get('/api/card/:id', (req, res) => cardController.getCard(req, res));
+app.put('/api/card', (req, res) => cardController.updateCard(req, res));
+app.delete('/api/card/:id', (req, res) => cardController.removeCard(req, res));
 
-app.post('/api/collection', async (req, res) =>
+app.post('/api/collection', (req, res) =>
   collectionController.addCollection(req, res)
 );
-app.get('/api/collections/user/:id', async (req, res) =>
+app.get('/api/collections/user/:id', (req, res) =>
   collectionController.getCollections(req, res)
 );
-app.get('/api/collection/:id', async (req, res) =>
+app.get('/api/collection/:id', (req, res) =>
   collectionController.getCollection(req, res)
 );
-app.put('/api/collection', async (req, res) =>
+app.put('/api/collection', (req, res) =>
   collectionController.updateCollection(req, res)
 );
-app.delete('/api/collection/:id', async (req, res) =>
+app.delete('/api/collection/:id', (req, res) =>
   collectionController.removeCollection(req, res)
 );
 
